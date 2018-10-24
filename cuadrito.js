d48b47464afa0e861ff732b8dd8339e4335b0d63
Gabriel Diaz <fungdc@hotmail.com>
2018-10-23 21:42:08 -0500

    cuadrito y adelanto de la pagina

---
+++ cuadrito.js
@@ -0,0 +1,8 @@
+function movimiento (strDistancia){
+    this.div =strDistancia
+}
+
+for (let strDistancia = 0; index < 150; index++) {
+    const element = array[index];
+    
+}
---
+++ unoalveinte.js
@@ -0,0 +1,10 @@
+
+for (var intNum = 0; intNum < 30 ; intNum++) {
+    document.write (intNum + "</br>");
+    alert ("llegó a "+ intNum)
+    if (intNum == 29) {
+    break;
+    }
+
+   
+}alert ("no ha llegado a"+ intNum ++)
